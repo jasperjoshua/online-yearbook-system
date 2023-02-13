19d9d914dd9bd133c31f08dff75e66fbe86ddd4f
chypacs <chylene.saraspe.learn@gmail.com>
2023-02-13 13:20:44 +0800

    Menu adjustment

--- views/menu_create.php
+++ views/menu_create.php
@@ -3,7 +3,7 @@
         <div class="container-xxl position-relative p-0">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-4 px-lg-5 py-3 py-lg-0">
                 <a href="index.php" class="navbar-brand p-0">
-                    <h1 class="text-primary m-0"><i class="fas fa-graduation-cap me-3"></i>BISU - Balilihan Campus</h1>
+                    <h1 class="text-primary m-0"><i class="fas fa-graduation-cap me-3"></i>BISU - BC Yearbook</h1>
                     <!-- <img src="img/logo.png" alt="Logo"> -->
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -15,18 +15,28 @@
                             <?php if (!isset($_GET['type']) && !isset($_GET['batch'])): ?>
                                     active
                                 <?php endif; ?>
-                        ">Create</a>
+                        ">
+                            <i class="bi bi-file-earmark-plus"></i>
+                            Create Yearbook
+                        </a>
                         <a href="upload.php?type=courses" class="nav-item nav-link 
                             <?php if (isset($_GET['type']) && $_GET['type'] == 'courses'): ?>
                                 active
                             <?php endif; ?>
-                        ">Upload Courses</a>
+                        ">
+                            <i class="bi bi-cloud-upload"></i>
+                            Upload Courses
+                        </a>
                         <div class="nav-item dropdown">
                             <a href="#" class="nav-link dropdown-toggle  
                                 <?php if (isset($_GET['batch']) && $_GET['batch'] != ''): ?>
                                     active
                                 <?php endif; ?>
-                            " data-bs-toggle="dropdown">Yearbooks</a>
+                            " data-bs-toggle="dropdown"
+                            >
+                                <i class="bi bi-pencil-square"></i>
+                                Edit Yearbook
+                            </a>
                             <div class="dropdown-menu m-0">
                                 <?php foreach ($_POST['draft_ybooks'] as $yearbook): ?>
                                     <a href="draft.php?batch=<?php echo $yearbook['Batch'] ?>"  class="dropdown-item ">
