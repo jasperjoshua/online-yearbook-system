13e29fc2725fb9de45e90df6ff939b4a5f830812
chypacs <chylene.saraspe.learn@gmail.com>
2023-02-21 22:23:13 +0800

    display draft section menus as side bar

---
+++ views/tpl_draft_sections.php
@@ -0,0 +1,43 @@
+
+
+                
+                <ul class="nav nav-pills nav-fill flex-column col-2 ml-4" style="display: inline-block; text-align: justify;" id="myTab" role="tablist">
+                    <?php foreach ($_POST['sections'] as $type => $page_type): ?>
+                    <li class="nav-item text-center" role="presentation" style="disaply: block;">
+                        <button class="nav-link 
+                            <?php if ($_POST['active'] == $type): ?>
+                                active
+                            <?php endif; ?>
+                            " data-bs-toggle="tab" type="button" role="tab" aria-selected="false" 
+                            id="<?php echo $type ?>" data-bs-target="#<?php echo $type ?>-pane" aria-controls="<?php echo $type ?>-pane" >
+                            <?php echo $_POST[$type]['title'] ?>                                               
+                        </button>
+                    </li>
+                    <?php endforeach; ?>
+                    <li class="nav-item dropdown " style="disaply: block;">
+                        <a class="nav-link 
+                            <?php if ($_POST['active'] == 'image'): ?>
+                                active
+                            <?php endif; ?>
+                            dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                            Background Images
+                        </a>
+                        <div id="bg_images-select" class="dropdown-menu">
+                            <?php foreach ($_POST['bg_images'] as $img_type => $images): ?>
+                                <?php foreach ($images as $type => $img_title): ?>
+                                    <a class="dropdown-item 
+                                        <?php if (isset($_POST['image_type']) && $_POST['image_type'] == $type): ?>
+                                            selected
+                                        <?php endif; ?>
+                                        " href="#<?php echo $type ?>-pane" id="<?php echo $type ?>"
+                                        data-bs-toggle="tab" role="tab" aria-selected="false" aria-controls="<?php echo $type ?>-pane">
+                                        <?php echo $img_type.' '.$img_title ?>  
+                                    </a>
+                                <?php endforeach; ?>
+                                <?php if ($img_type == 'Common'): ?>
+                                    <div class="dropdown-divider"></div>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </div>
+                    </li>
+                </ul>
--- views/ui_draft_theme.php
+++ views/ui_draft_theme.php
@@ -53,49 +53,9 @@
         </div>
         <div class="container py-5">
             <hr class="hr-blurry" />
-            <div class="row">
-                
-                <ul class="nav nav-pills nav-fill flex-column col-2" style="display: inline-block; text-align: left;" id="myTab" role="tablist">
-                    <?php foreach ($_POST['sections'] as $type => $page_type): ?>
-                    <li class="nav-item" role="presentation">
-                        <button class="nav-link 
-                            <?php if ($_POST['active'] == $type): ?>
-                                active
-                            <?php endif; ?>
-                            " data-bs-toggle="tab" type="button" role="tab" aria-selected="false" 
-                            id="<?php echo $type ?>" data-bs-target="#<?php echo $type ?>-pane" aria-controls="<?php echo $type ?>-pane" >
-                            <?php echo $_POST[$type]['title'] ?>                                               
-                        </button>
-                    </li>
-                    <?php endforeach; ?>
-                    <li class="nav-item dropdown ">
-                        <a class="nav-link 
-                            <?php if ($_POST['active'] == 'image'): ?>
-                                active
-                            <?php endif; ?>
-                            dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                            Background Images
-                        </a>
-                        <div id="bg_images-select" class="dropdown-menu">
-                            <?php foreach ($_POST['bg_images'] as $img_type => $images): ?>
-                                <?php foreach ($images as $type => $img_title): ?>
-                                    <a class="dropdown-item 
-                                        <?php if (isset($_POST['image_type']) && $_POST['image_type'] == $type): ?>
-                                            selected
-                                        <?php endif; ?>
-                                        " href="#<?php echo $type ?>-pane" id="<?php echo $type ?>"
-                                        data-bs-toggle="tab" role="tab" aria-selected="false" aria-controls="<?php echo $type ?>-pane">
-                                        <?php echo $img_type.' '.$img_title ?>  
-                                    </a>
-                                <?php endforeach; ?>
-                                <?php if ($img_type == 'Common'): ?>
-                                    <div class="dropdown-divider"></div>
-                                <?php endif; ?>
-                            <?php endforeach; ?>
-                        </div>
-                    </li>
-                </ul>
-               
+            <div class="row m-2">
+                <?php require 'views/tpl_draft_sections.php' ?>
+
                 <div class="tab-content col-10" id="myTabContent">              
                     <?php foreach ($_POST['data_list'] as $type => $page_type): ?>
                         <div class="tab-pane fade
