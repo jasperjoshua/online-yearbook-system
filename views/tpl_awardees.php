d9294f6904b32d6f0f19899eb58dd2ae500cda69
chypacs <chylene.saraspe.learn@gmail.com>
2023-02-21 22:33:41 +0800

    added checking whne no data

--- views/tpl_awardees.php
+++ views/tpl_awardees.php
@@ -8,6 +8,7 @@
         <div class="container-fluid">
             <div class="row m-0 mt-2">
                 <div class="col-sm-12">
+                <?php if (isset($_POST['data']['center'])): ?>
                     <?php foreach($_POST['data']['center'] as $value): ?>
                     <div class="row mt-3">
                         <p class="small text-dark m-0"><strong><?php echo $value['Full_Name'] ?></strong></p>
@@ -21,8 +22,10 @@
                         </p>
                     </div>
                     <?php endforeach; ?>
+                <?php endif; ?>
                 </div>
                 <div class="col-sm-6 mt-0">
+                <?php if (isset($_POST['data']['left'])): ?>
                     <?php foreach($_POST['data']['left'] as $value): ?>
                     <div class="row mt-3" style="text-align:center;">
                         <p class="small text-dark m-0"><strong><?php echo $value['Full_Name'] ?></strong></p>
@@ -36,8 +39,10 @@
                         </p>
                     </div>
                     <?php endforeach; ?>
+                <?php endif; ?>
                 </div>
                 <div class="col-sm-6 m-0">
+                <?php if (isset($_POST['data']['right'])): ?>
                     <?php foreach($_POST['data']['right'] as $value): ?>
                     <div class="row text-right mt-4" style="text-align:center;">
                         <p class="small text-dark m-0"><strong><?php echo $value['Full_Name'] ?></strong></p>
@@ -51,8 +56,10 @@
                         </p>
                     </div>
                     <?php endforeach; ?>
+                <?php endif; ?>
                 </div>
                 <div class="col-sm-12">
+                <?php if (isset($_POST['data']['bottom'])): ?>
                     <?php foreach($_POST['data']['bottom'] as $value): ?>
                     <div class="row mt-3">
                         <p class="small text-dark m-0"><strong><?php echo $value['Full_Name'] ?></strong></p>
@@ -66,6 +73,7 @@
                         </p>
                     </div>
                     <?php endforeach; ?>
+                <?php endif; ?>
                 </div>
             </div>
         </div>
